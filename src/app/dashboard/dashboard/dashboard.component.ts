73af7912b593e914ee1eddb7c6e623479515f6ef
rami.benmessaoud@esprit.tn <57332207+Ramiben1993@users.noreply.github.com>
2022-03-03 23:27:27 +0100

    add forms

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {TaskServiceService} from "../../task-service.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  private router: any;
 
-  constructor() { }
+  constructor( private taskservice :TaskServiceService, router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  logoutDash(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+   // this.router.navigateByUrl("/admin");
+    this.router.navigateByUrl("/admin");
   }
 
 }
